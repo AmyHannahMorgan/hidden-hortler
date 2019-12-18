1ca8111ed30e726b4606535debf254bef91af54e
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-18 18:56:25 +0000

    wrote generator functions

--- generators.js
+++ generators.js
@@ -1,11 +1,15 @@
 function generateID() {
-
+    let base10 = rng(17592186044416, 281474976710655);
+    return base10.toString(16)
 }
 
 function generateGameCode() {
-
+    let base10 = rng(4096, 65535);
+    return base10.toString(16)
 }
 
 function RNG(min, max) {
-
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
