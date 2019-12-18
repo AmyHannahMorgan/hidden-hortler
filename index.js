8229449864fc358852cfd717abc14d7124306055
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-18 17:33:52 +0000

    added some comments for organisation

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+//website serving
 app.use(express.static('static'));
+
+//API
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
