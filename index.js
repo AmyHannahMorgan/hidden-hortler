9a1a10855de20f37116a638cfa528dc8c96e1bde
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-19 13:11:04 +0000

    added logs and response

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.use(express.static('static'));
 
 //API
 app.post('/api/host', (req, res) => {
-    games.push(gen.gameObject(games));
+    console.log({req});
+    console.log(req.ips);
+    console.log(`Host game request recieved from: ${req.ip}`);
+    let obj = gen.gameObject(games, req.ip);
+    console.log(`Game created with the id: ${obj.gameId} \nand the code: ${obj.gameCode}`);
+    games.push(obj);
+
+    res.status(200).send(JSON.stringify(obj));
+
 });
 
 app.get(/\/api\/play\/\?code=[a-f0-9]{4}/, (req, res) => {
@@ -23,7 +31,3 @@ app.get(/\/api\/signaling\/\?id=[a-f0-9]{12}/, (req, res) => {
 app.listen(port, () => console.log(`Listening on port: ${port}`));
 
 //helper functions
-
-module.exports = {
-    genUnique: genUnique
-};
