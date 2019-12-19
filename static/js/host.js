bfcdbe6d09b38bc325c7b9e8edcfb905d7b2b5a2
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-19 15:33:41 +0000

    wrote code to insert game code

--- static/js/host.js
+++ static/js/host.js
@@ -1,8 +1,14 @@
 const apiUrl = window.location.origin + '/api';
 const hostReq = new XMLHttpRequest();
 
+const gameCodeElement = document.querySelector('#gameCode');
+let gameObject;
+
 hostReq.addEventListener('load', (e) => {
     console.log(e);
+    gameObject = JSON.parse(e.target.responseText);
+    gameCodeElement.innerHTML = gameObject.gameCode;
 });
 hostReq.open('POST', apiUrl + '/host');
 hostReq.send();
+
