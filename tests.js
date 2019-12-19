0234bb59c1d949fd6fc0d30187be28e93146223e
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-19 12:41:15 +0000

    wrote code to test genUnique

--- tests.js
+++ tests.js
@@ -1,7 +1,19 @@
 const index = require('./index');
 const generators = require('./generators');
 const tests = {
-    genUnique = () => {
-        
+    genUnique: () => {
+        let ar = [1, 3, 5, 6, 2, 9];
+        let func = () => RNG(1, 10);
+        let value = index.genUnique(ar, func);
+        console.log({ar})
+        console.log({value})
     }
 }
+
+function RNG(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+tests.genUnique();
