e1a72b69d191bd4c04b410ced268a4d77620e5a2
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-19 13:17:54 +0000

    wrote initial request code

---
+++ static/js/host.js
@@ -0,0 +1,8 @@
+const apiUrl = window.location.origin + '/api/';
+const hostReq = new XMLHttpRequest();
+
+hostReq.addEventListener('load', (e) => {
+    console.log(e);
+});
+hostReq.open('POST', apiUrl + '/host');
+hostReq.send();
