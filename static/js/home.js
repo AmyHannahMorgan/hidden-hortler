9fa438707c7f0b314d2a29f7fec1a48ddf670c51
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-21 10:10:21 +0000

    wrote code to open modal and send req to server

---
+++ static/js/home.js
@@ -0,0 +1,58 @@
+const apiUrl = window.location.origin + '/api';
+
+const playButton = document.querySelector('#play');
+const playModal = document.querySelector('#playModal');
+const playModalClose = playModal.querySelector('.closeContainer');
+const playModalInput = playModal.querySelector('#gameCodeInput');
+const playModalJoin = playModal.querySelector('#joinButton');
+const playModalError = playModal.querySelector('#errorText');
+
+const hostButton = document.querySelector('#host');
+
+playButton.addEventListener('click', () => {
+    playModal.classList.add('active');
+});
+
+playModalClose.addEventListener('click', () => {
+    playModal.classList.remove('active');
+});
+
+playModalJoin.addEventListener('click', () => {
+    if(checkCodeInput(playModalInput.value)) {
+        let req = new XMLHttpRequest()
+        req.addEventListener('load', (e) => {
+            console.log(e);
+            switch(e.target.status) {
+                case 404:
+                    playModalError.innerHTML = e.target.responseText;
+                    break;
+                case 200:
+                    window.location.href = e.target.responseURL;
+                    break;
+            }
+        });
+
+        req.open('GET', apiUrl + '/play/' + playModalInput.value.trim());
+        req.send();
+    }
+    else {
+        playModalError.innerHTML = 'Game code is invalid. <br> Reminder: codes are case sensitive'
+    }
+});
+
+function checkCodeInput(string) {
+    string = string.trim();
+    let check = string.match(/^[a-f0-9]{4}$/);
+
+    if(check !== null) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+hostButton.addEventListener('click', () => {
+    window.location.href = window.location.origin + '/host';
+})
+
