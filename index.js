fae790747a5a093a900382444b103a062f19cfdf
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-26 15:24:13 +0000

    required ws

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gen = require('./generators');
 const express = require('express');
+const webSocket = require('ws');
 const app = express();
 const port = process.env.PORT || 3000;
 const games = [];
