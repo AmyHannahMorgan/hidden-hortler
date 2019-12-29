d43e3365bd1122b33629a8f27a12ac57a37b8d1e
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-29 21:00:58 +0000

    began writing playboard object creation

---
+++ static/js/client.js
@@ -0,0 +1,17 @@
+const playBoards = document.querySelectorAll('.playBoard');
+const playBoardObjs = [];
+const loadingScreen = document.querySelector('#loading');
+
+for(let i = 0; i < playBoards; i++) {
+    switch (playBoards[i].id) {
+        case 'nameSelect':
+            playBoardObjs.push(() => {
+                console.log(playBoards[i]);
+                return 1;
+            });
+            break;
+        default:
+            console.log('i got here');
+            break;
+    }
+}
