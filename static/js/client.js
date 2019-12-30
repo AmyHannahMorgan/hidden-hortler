42c532440b4b4beff34a20049d705b2188beed75
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-30 10:54:34 +0000

    setup ws connection to server

--- static/js/client.js
+++ static/js/client.js
@@ -16,6 +16,8 @@ const playBoards = document.querySelectorAll('.playBoard');
 const playBoardObjs = [];
 const loadingScreen = document.querySelector('#loading');
 
+let ws = new WebSocket(`ws://${window.location.host}`);
+
 for(let i = 0; i < playBoards.length; i++) {
     playBoardObjs.push(new PlayBoard(playBoards[i]));
 }
