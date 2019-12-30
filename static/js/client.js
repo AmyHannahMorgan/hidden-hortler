310f18f58c7132586fcf91ec14f79ed4e499f73a
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-30 12:52:36 +0000

    began function to handle responses from server

--- static/js/client.js
+++ static/js/client.js
@@ -16,7 +16,46 @@ const playBoards = document.querySelectorAll('.playBoard');
 const playBoardObjs = [];
 const loadingScreen = document.querySelector('#loading');
 
+let myId;
+
 let ws = new WebSocket(`ws://${window.location.host}`);
+
+ws.onopen = () => {
+    let msgObj = {
+        type: 2,
+    }
+    ws.send(JSON.stringify(msgObj));
+};
+
+ws.onmessage = (messsage) => {
+    try {
+        let msgJson = JSON.parse(messsage);
+
+        if(msgJson.type !== undefined) {
+
+        }
+        else if(msgJson.result !== undefined) {
+            if(msgJson.result === 0) {
+                switch (msgJson.body.type) {
+                    case 2:
+                        myId = msgJson.body.id;
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+            else if(msgJson.result === 1) {
+                console.log(msgJson.body.error);
+            }
+        }
+        else {
+            console.log(msgJson);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 for(let i = 0; i < playBoards.length; i++) {
     playBoardObjs.push(new PlayBoard(playBoards[i]));
