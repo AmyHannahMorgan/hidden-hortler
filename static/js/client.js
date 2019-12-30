0bbde63da857a81b667def621110e85169647a8d
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-12-30 10:40:35 +0000

    created base of PlayBoard class

--- static/js/client.js
+++ static/js/client.js
@@ -1,17 +1,21 @@
+class PlayBoard {
+    constructor(element) {
+        this.element = element;
+
+        switch(element.id) {
+            case 'nameSelect':
+                break;
+            default:
+                console.log(`unrecognised id: ${element.id}`);
+                break;
+        }
+    }
+}
+
 const playBoards = document.querySelectorAll('.playBoard');
 const playBoardObjs = [];
 const loadingScreen = document.querySelector('#loading');
 
 for(let i = 0; i < playBoards.length; i++) {
-    switch (playBoards[i].id) {
-        case 'nameSelect':
-            playBoardObjs.push(() => {
-                console.log(playBoards[i]);
-                return 1;
-            });
-            break;
-        default:
-            console.log('i got here');
-            break;
-    }
+    playBoardObjs.push(new PlayBoard(playBoards[i]));
 }
